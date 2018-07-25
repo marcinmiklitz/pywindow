fc520c63b29c4086be26694990eaf51058c27f57
Marcin Miklitz <marcin.miklitz@gmail.com>
2018-07-25 14:37:41 +0100

    Updated pywindowx on pypi.org

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from distutils.core import setup
+#from setuptools import setup
 
 setup(name='pywindow',
       author='Marcin Miklitz',
       author_email='marcin.miklitz@gmail.com',
       url='https://www.github.com/marcinmiklitz/pywindow',
-      version='0.0.1',
+      version='0.0.2',
       packages=['pywindow'])
