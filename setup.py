72629b68411b55bdfdd1cae115da6bfec58b308e
Lukas Turcani <lukasturcani93@gmail.com>
2019-10-22 08:31:35 +0200

    Added requirements.

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
-#from distutils.core import setup
 from setuptools import setup
 
-setup(name='pywindow',
-      author='Marcin Miklitz',
-      author_email='marcin.miklitz@gmail.com',
-      url='https://www.github.com/marcinmiklitz/pywindow',
-      version='0.0.2',
-      packages=['pywindow'])
+setup(
+    name='pywindowx',
+    author='Marcin Miklitz',
+    author_email='marcin.miklitz@gmail.com',
+    url='https://www.github.com/marcinmiklitz/pywindow',
+    version='0.0.4',
+    packages=['pywindow'],
+    install_requires=[
+        'numpy',
+        'scipy',
+        'scikit-learn',
+    ],
+)
