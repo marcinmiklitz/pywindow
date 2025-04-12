4b62c57593575255804bd04d41f6a2427887cdb0
Andrew Tarzia <andrew.tarzia@gmail.com>
2025-04-12 22:21:15 +0200

    Ruff fixes and handling new versions of numpy/scipy.

--- src/pywindow/__init__.py
+++ src/pywindow/__init__.py
@@ -1,10 +1,12 @@
-#!/usr/bin/env python3
-from .tables import *
-from .io_tools import *
-from .utilities import *
-from .molecular import *
-from .trajectory import *
-# from .postprocess import *
-# from .shape import *
+"""pywindow module."""
 
-name = 'pywindow'
+from .io_tools import *
+from .molecular import *
+from .tables import *
+from .trajectory import *
+from .utilities import *
+
+name = "pywindow"
+
+
+__all__ = []
